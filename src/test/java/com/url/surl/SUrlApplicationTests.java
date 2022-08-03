d9c426db7d63e4fe39216486ecc7b4ba0b3cbc4a
kamaek <o-grank@yandex.ru>
2022-08-03 19:23:01 +0300

    Generate project using Spring Initializer

---
+++ src/test/java/com/url/surl/SUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.surl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
